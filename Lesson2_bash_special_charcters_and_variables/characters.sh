19052048715ab0b4b18914a23416d3856ec8e025
norahara <toxirovsurat321@gmail.com>
2025-08-14 12:48:27 +0500

    Done

--- Lesson2_bash_special_charcters_and_variables/characters.sh
+++ Lesson2_bash_special_charcters_and_variables/characters.sh
@@ -58,3 +58,70 @@ ls -l ../..
 echo $null
 
 
+# vergul vazifasi amallarni ajratishga yordam beradi
+
+let "t2 = ((nine = 9, 15 / 3))"
+# Set "a = 9" and "t2 = 15 / 3"
+echo $nine $t2
+#  "," fayllar bilan ham amallar bajariladi
+
+ls -la /{,usr/}bin/*calc
+#  bu yerda  usr/bin va bin papkasida mavjud bo'lgan calc bilan tugaydigan fayllar ro`yxatini chiqaradi
+
+
+#  string bo'yicha ^ va ,
+
+String="buSuzTest"
+echo ${String} # buSuzTest ni o'zi chiqadi
+echo ${String^} # birinchi harf bosh harda bo'ladi
+echo ${String,} # birinichi harfni kichik harfda qiladi
+echo ${String^^} # barcha harflar bosh harfda
+echo ${String,,} # barcha harflar kichik harfda
+
+#  \ -> backslash escape istisnolar kiritishda qo`llaniladi asosan echo va sed da foydalaniladi
+#  -e optioni orqali bajariladi
+
+# \n -> yangi qator
+echo -e "\n salom" # bir qato tashlab chiqaradi
+echo -e "\t salom" # \t -> tab 
+echo -e "\v salom" # vertikal tab
+echo -e "salom\b" # backspace
+echo -e "\a salom" # alert
+
+# / bu esa fayl yo'lini ko'rsatish uchun foydalaniladi
+
+ls -1 ../ # /katalog/fayl_ichidagi_boshqa_katalog
+
+#  `` ushbu backquotes ichida yozilgan matn kod sifatida olinadi
+cod1="du -h ." 
+echo -e "\n $cod1"
+cod2=`du -h .`
+echo -e "\n $cod2"
+
+#  : > va  > belgilari
+# echo > file_noi ko'rinishida faylga ma'lumot yoziladi
+# : > faylni bo`shatadi
+
+# touch text.txt # bir martta yaratib olish uchun 
+echo "salom bu hozir bu faylda ma'lumot bor" > text.txt # faylga matn yoziladi
+cat text.txt # fayl uqiladi
+: > text.txt # fayl bushatiladi
+cat text.txt
+
+# * bu barcha fayllarni yoki barcha *.txt kabi kengaytmalarni ifodalashda qo`llaniladi
+
+echo * # faylning tarkibini ko'rsatadi
+echo * .sh # barcha .sh bilan tugaydigan fayllarni ko`rsatadi
+echo [cTv]* # barcha cTtv harfi bilan foydalaniladigan fayllarni ko'rsatadi
+
+echo $* # barcha argumentlar
+echo $@ # $* bilan bir xil
+echo $# # barcha argumentlar soni
+# misol sifatida ./characters.sh first_arg 12 third_arg
+
+# $? exit status scriptdan chiqish holati oxirgi bajarilgan komanda haqida
+# agar xatolik bo`lsa scriptning oxirgi komandasida qandaydir son qaytaradi, misol uchun: No such file or directory 1 yoki command not found 127
+#  muvofiqqiyatli bulsa o ciqadi
+
+echo $?
+
